ae87b3d8673f4ff3e15c1c72e527116b872248b9
mohafizz <mohafizz@gmail.com>
2018-03-13 23:20:30 +0800

    Initial commit

--- app.js
+++ app.js
@@ -5,12 +5,44 @@ const bodyParser = require("body-parser");
 const encode = require("./demo/encode");
 const decode = require("./demo/decode");
 
+const existingURLs = [
+  { id: "1", url: "www.google.com", hash: "MQ==" },
+  { id: "2", url: "www.facebook.com", hash: "Mg==" },
+  { id: "3", url: "www.test.com", hash: "NA==" }
+];
+
 const app = express();
 app.use(bodyParser.json());
 
 // TODO: Implement functionalities specified in README
 app.get("/", function(req, res) {
   res.send("Hello world!");
+});
+
+app.post("/shorten-url", function(req, res) {
+  const url = req.body.url;
+  let encodedResult = encode(url, existingURLs);
+  let newURL = {
+    id: "3",
+    url: url,
+    hash: encodedResult
+  };
+  existingURLs.push(newURL);
+  res.send(
+    `${newURL.id}) ${newURL.url} is created with new hash: ${newURL.hash}`
+  );
+  res.end();
+});
+
+app.post("/expand-url", function(req, res) {
+  const hashUrl = req.body.hash;
+  try {
+    let decodedResult = decode(hashUrl, existingURLs);
+  } catch (error) {
+    res.status(error.status || 404);
+  }
+  res.send(`There is no long URL registered for hash value "${hashUrl}"`);
+  res.end();
 });
 
 // catch 404 and forward to error handler
@@ -28,7 +60,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  console.log(err);
+  res.send("error");
 });
 
 module.exports = app;
