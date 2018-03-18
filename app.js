b559924e658d6fd2ac769c512e4fb10035be8d1d
mohafizz <mohafizz@gmail.com>
2018-03-18 12:55:13 +0800

    added fresh start when mongoose DB is connected.

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ const existingURLs = [];
 const dbUrl = process.env.MONGODB_URI;
 mongoose.connect(dbUrl, {}).then(async () => {
   console.log("Connected to mongo database at " + dbUrl);
+  URLs.remove({}, function() {
+    console.log("URL collection removed");
+  });
+  Counter.remove({}, function() {
+    console.log("Counter collection removed");
+    var counter = new Counter({ _id: "url_count", count: 10000 });
+    counter.save(function(err) {
+      if (err) return console.error(err);
+      console.log("counter inserted");
+    });
+  });
 });
 
 // TODO: Implement functionalities specified in README
