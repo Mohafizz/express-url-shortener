d1b151163a81b49fb882d66ce87427ac6f82debf
mohafizz <mohafizz@gmail.com>
2018-03-19 11:12:26 +0800

    refactor pre-save function to async await

--- models/urls.js
+++ models/urls.js
@@ -7,13 +7,13 @@ var urlSchema = new mongoose.Schema({
   created_at: ""
 });
 
-urlSchema.pre("save", function(next) {
+urlSchema.pre("save", async function(next) {
   console.log("running pre-save");
   var doc = this;
-  Counter.findByIdAndUpdate(
+  await Counter.findByIdAndUpdate(
     { _id: "url_count" },
     { $inc: { count: 1 } },
-    function(err, counter) {
+    await function(err, counter) {
       if (err) return next(err);
       console.log(counter);
       console.log(counter.count);
